e24014043380def0bcefb3fef86011f17a5f8bdb
N. Omalley Segbee Jr <segbeejr67@gmail.com>
2023-07-04 19:36:58 +0000

    Assignment Commit

--- script.js
+++ script.js
@@ -4,31 +4,43 @@ let seconds = 0;
 let milliseconds = 0;
 let timerDisplay = document.getElementById('timer-display');
 
-// implement the below funtion using the setInteral funtion
 function startTimer() {
+  timer = setInterval(function() {
+    milliseconds++;
+    if (milliseconds === 10) {
+      seconds++;
+      milliseconds = 0;
+    }
 
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    }
+
+    timerDisplay.innerText = formatTime(minutes, seconds, milliseconds);
+  }, 100);
 }
 
-
-
 function formatTime(minutes, seconds, milliseconds) {
-    return (
-        (minutes < 10 ? '0' + minutes : minutes) +
-        ':' +
-        (seconds < 10 ? '0' + seconds : seconds) +
-        ':' +
-        (milliseconds < 10 ? '0' + milliseconds : milliseconds)
-    );
+  return (
+    (minutes < 10 ? '0' + minutes : minutes) +
+    ':' +
+    (seconds < 10 ? '0' + seconds : seconds) +
+    ':' +
+    (milliseconds < 10 ? '0' + milliseconds : milliseconds)
+  );
 }
 
 function pauseTimer() {
-    clearInterval(timer);
+  clearInterval(timer);
 }
 
 function resetTimer() {
-    clearInterval(timer);
-    minutes = 0;
-    seconds = 0;
-    milliseconds = 0;
-    timerDisplay.textContent = '00:00:00';
+  clearInterval(timer);
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+  timerDisplay.textContent = '00:00:00';
 }
+
+
